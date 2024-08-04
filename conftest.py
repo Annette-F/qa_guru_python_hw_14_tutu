2732f3fd5dffc35faebc720803a7cbae94de320d
Anna Fedorova <anna.fedorova.testing@gmail.com>
2024-08-04 22:24:01 +0300

    раскомментирован часть conftest

--- conftest.py
+++ conftest.py
@@ -8,14 +8,14 @@ from selene import browser
 from utils import attach
 from dotenv import load_dotenv
 
-# DEFAULT_BROWSER_VERSION = '100.0'
-#
-#
-# def pytest_addoption(parser):
-#     parser.addoption(
-#         '--browser_version',
-#         default='100.0'
-#     )
+DEFAULT_BROWSER_VERSION = '100.0'
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
 
 
 # @pytest.fixture(scope='session', autouse=True)
@@ -25,32 +25,32 @@ from dotenv import load_dotenv
 
 @pytest.fixture(scope='function', autouse=True)
 def setting_browser(request):
-    # browser_version = request.config.getoption('--browser_version')
-    # browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    browser.config.base_url = 'https://www.tutu.ru'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    #
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": browser_version,
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
 
-    # options.capabilities.update(selenoid_capabilities)
+    options.capabilities.update(selenoid_capabilities)
     #
     # login = os.getenv('LOGIN')
     # password = os.getenv('PASSWORD')
     #
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-    #     options=options)
-    #
-    # browser.config.driver = driver
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.base_url = 'https://www.tutu.ru'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+    browser.config.driver = driver
 
     yield browser
 
